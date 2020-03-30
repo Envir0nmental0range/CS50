846f83c15ee189f47d3e3500fc8b60b28390f8ae
Envir0nmental0range <60448178+Envir0nmental0range@users.noreply.github.com>
2020-03-30 12:12:20 +0100

    Update cash.c

--- cash.c
+++ cash.c
@@ -35,7 +35,60 @@ int main()
     while(change >= 0.05)
         {
             counter ++;
-            change -= 0.05;
+            change -= 0.05;#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    int counter; 
+    float change;
+    counter = 0;
+    do //doWhile loop to get positive float
+    {
+    change = get_float("Change owed: ");
+    change = round(change * 100); // change set to a 2dp value
+    change = change / 100;
+    }
+    while(change < 0);
+
+    while(change >= 0.25) // while loop to decrement by 25c
+    {
+        counter ++;
+        change -= 0.25;
+    }
+    
+    change = round(change * 100); // change set to a 2dp value
+    change = change / 100;
+    
+    while(change >= 0.1)
+    {
+        counter ++;
+        change -= 0.1;
+    } 
+    
+    change = round(change * 100); // change set to a 2dp value
+    change = change / 100;
+    
+    while(change >= 0.05)
+    {
+        counter ++;
+        change -= 0.05;
+    }   
+    
+    change = round(change * 100); // change set to a 2dp value
+    change = change / 100;
+    
+    while(change > 0.0)
+    {
+        counter ++;
+        change -= 0.01;
+        change = round(change * 100); // change set to a 2dp value
+        change = change / 100;
+    }
+    printf("%d\n", counter);
+    return 0;
+}
+
         }   
     
     change = round(change * 100); // change set to a 2dp value
