97b3b784da0c253bc2ad0f1c2c4803c6b6a9ef37
Envir0nmental0range <60448178+Envir0nmental0range@users.noreply.github.com>
2020-03-30 10:46:57 +0100

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+int main() {
+    int i, j, rows;
+    rows = 0;
+    do
+        {
+    printf("Enter number of rows: ");
+        scanf("%d", &rows);
+
+    }while((rows > 8) || (rows < 1));
+           
+    for (i=1; i<=rows; ++i) {
+        for (j=rows - i; j>=1; --j)
+        { printf(" "); }
+        for (j=1; j<=i; ++j)
+        { printf("#"); }
+        printf("\n");
+    }
+    return 0;
+}
+
